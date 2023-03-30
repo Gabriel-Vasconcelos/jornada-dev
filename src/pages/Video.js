bba17ae0f91d8d3b3cc509197eb416cdbe3836a9
Gabriel-Vasconcelos <gabrieltotal6@gmail.com>
2023-03-30 12:57:18 -0300

    começando a codar o componente de video

---
+++ src/pages/Video.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Video.css"
+
+const Video = () => {
+    return(
+        <div className='video'>
+            Componente Vídeo
+
+            <video className='video__player'>
+                
+            </video>
+        </div>
+    )
+}
+
+export default Video ;

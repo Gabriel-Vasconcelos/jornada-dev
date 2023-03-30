541f960dd7b3760e468a3ad8d4c06364905f77e3
Gabriel <62850565+Gabriel-Vasconcelos@users.noreply.github.com>
2023-03-30 15:16:47 -0300

    finalizando a aula 2

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import "./Video.css"
+import React, { useRef, useState } from 'react';
+import "./Video.css";
 
 const Video = () => {
+    
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+
+    const handdleStart = () => {
+        if(play){
+            videoRef.current.pause()
+            setPlay(false)
+        }else{
+            videoRef.current.play()
+            setPlay(true)
+        }
+    }
+
+
     return(
         <div className='video'>
             Componente Vídeo
 
-            <video className='video__player'>
+            <video 
+                className='video__player'
+                ref={videoRef}
+                onClick={handdleStart}
+                loop
+                src = "https://v16-webapp-prime.tiktok.com/video/tos/useast2a/tos-useast2a-pve-0068/oM7gIFzgyIkOCMgEpAEfl4KhUzzMAXAOMotFSa/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1314&bt=657&cs=0&ds=3&ft=_RwJrBiTq8Zmo3eVCc_vjz_x8AhLrus&mime_type=video_mp4&qs=0&rc=NWhnaWVmZDpoZzdlM2dnNUBpanVmOTc6ZjlkajMzNzczM0A2MmItMTNfX18xYF4xYDNeYSNtcWotcjRfXmRgLS1kMTZzcw%3D%3D&btag=80000&expire=1680220396&l=2023033017530676C26F7B312656454838&ply_type=2&policy=2&signature=b5d32bff5572f2d6174003e3d9faf22c&tk=tt_chain_token"
+            >
                 
             </video>
         </div>
     )
 }
 
-export default Video ;
+export default Video;

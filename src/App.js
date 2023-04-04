74a445f48557d73e9bef8fc21f6b206dce6e33b4
Gabriel-Vasconcelos <gabrieltotal6@gmail.com>
2023-04-04 16:11:26 -0300

    finalizando aula 6

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import db from './config/firebase';
 import { collection, getDocs } from 'firebase/firestore/lite';
 
 function App() {
+  let maxHeight;
+  if(window.innerHeight <= 800){
+    maxHeight = window.innerHeight
+  }
 
-  const [video, setVideo] = useState([])
+  const [video, setVideo] = useState([]);
 
   async function getVideos() {
     const videosColletion = collection(db, 'videos')
@@ -20,7 +24,7 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + 'px' }}>
       <div className='app__videos'>
 
         {video.map((item) => {
